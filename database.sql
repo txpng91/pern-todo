9d55e7fe7427b38d2fa714f7eafad25ae1478471
txpng91 <petengarcia@gmail.com>
2024-06-15 13:24:31 -0400

    Different pool approach

--- database.sql
+++ database.sql
@@ -7,3 +7,4 @@ CREATE TABLE todos (
     description VARCHAR(255)
 );
 
+
--- db.js
+++ db.js
@@ -14,8 +14,23 @@ const proConfig = {
 };
 
 // Set up configurations
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
-);
+// const pool = new Pool(
+//   process.env.NODE_ENV === 'production' ? proConfig : devConfig
+// );
+
+// Adjustments
+
+const pool =
+  process.env.NODE_ENV === 'production'
+    ? new Pool({
+        connectionString: proConfig,
+
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      })
+    : new Pool({
+        connectionString: devConfig,
+      });
 
 module.exports = pool;
